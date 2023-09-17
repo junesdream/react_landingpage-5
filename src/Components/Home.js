2d2a6fae346c22d549377b2379e5b99eabbf7642
junesdream <norisori@gmail.com>
2023-09-18 00:49:32 +0200

    edited home compo

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,31 +1,38 @@
 import React from 'react'
 import "./Home.css";
 import Navbar from './Navbar'
+import BannerBg from "../Assets/banner-background.png"
 import { FiArrowRight } from "react-icons/fi";
 import HomeImg from "../Assets/home-image.png"
 
 
 const Home = () => {
   return (
-		<div className="home">
+		<div className="home-container">
 			<Navbar />
-            <div className="home-text-section">
-          <h1 className="primary-heading">
-            Your Favourite Food Delivered Hot & Fresh
-          </h1>
-          <p className="primary-text">
-            Healthy switcher chefs do all the prep work, like peeding, chopping
-            & marinating, so you can cook a fresh food.
-          </p>
-          <button className="secondary-button">
-            Order Now <FiArrowRight />{" "}
-          </button>
-        </div>
-        <div className="home-image-section">
-          <img src={HomeImg} alt="" />
-        </div>
-      </div>
-  )
+			<div className="home-banner-container">
+				<div className="home-bannerImage-container">
+					<img src={BannerBg} alt="" />
+				</div>
+				<div className="home-text-section">
+					<h1 className="primary-heading">
+						Your Favourite Food Delivered Hot & Fresh
+					</h1>
+					<p className="primary-text">
+						Healthy switcher chefs do all the prep work, like
+						peeding, chopping & marinating, so you can cook a fresh
+						food.
+					</p>
+					<button className="secondary-button">
+						Order Now <FiArrowRight />{" "}
+					</button>
+				</div>
+				<div className="home-image-section">
+					<img src={HomeImg} alt="" />
+				</div>
+			</div>
+		</div>
+  );
 }
 
 export default Home 
